6671712b006779f0caf1cbf0d71c2d6bca5da4ea
LWaters2002 <47858220+LWaters2002@users.noreply.github.com>
2022-04-05 13:01:58 +0100

    Added Earth Earth Ability

--- Hye - Indie Games Dev/Assets/EarthEarthSub.cs
+++ Hye - Indie Games Dev/Assets/EarthEarthSub.cs
@@ -2,32 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EarthEarthSub : MonoBehaviour
+public class EarthEarthSub : MonoBehaviour, IDamagable
 {
     public Rigidbody rb { get; private set; }
+    public float maxHealth; 
+    public float health { get; private set; }
+
+    ParticleSystem ps;
+
+    Animator an;
     public LayerMask mask;
 
-    float distance = .1f;
+    bool hasLanded = false;
+
+    float distance = .65f;
 
     public void Init(Vector3 dir, float force) 
     {
         rb = GetComponent<Rigidbody>();
+        ps = GetComponentInChildren<ParticleSystem>();
+        an = GetComponent<Animator>();
         GetComponentInChildren<CylinderCollider>().overlapEvent += ColliderOverlap; //Sets up overlap
+
+        health = maxHealth;
         
         rb.AddForce(dir * force, ForceMode.VelocityChange);
     }
 
-    private void Update()
+    private void Update()   
     {
-        if (Physics.Raycast(transform.position,Vector3.down*distance, mask))
+        if (hasLanded) { return; } 
+        //Checks if touching floor.
+        if (Physics.Raycast(transform.position,Vector3.down, distance,mask))
         {
-            rb.AddForce(Vector3.up*5f, ForceMode.VelocityChange);
+            rb.isKinematic = true;
+            ps.Play();
+            an.SetBool("HasLanded", true);
+            hasLanded = true;
         }
+   
+    }
+    public void TakeDamage(float damageAmount, StatusType damageType, Vector3 damagePos) 
+    {
+        health -= damageAmount;
+        if (health < 0) 
+        {
+            Destroy(gameObject);
+        }
+        lutils.SpawnDamageNumber(damagePos, damageAmount);
     }
 
     void ColliderOverlap(Collider col)
     {
-        rb.AddForce(Vector3.up * 5f, ForceMode.VelocityChange);
+        Enemy e = GetComponentInParent<Enemy>();
+        e.TakeDamage(10,StatusType.earth, transform.position);
     }
 
 
