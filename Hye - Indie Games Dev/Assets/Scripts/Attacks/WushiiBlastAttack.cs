6c684064ef8937ad7ec8ad53a9ffb2797cf30671
Liamo <liamwaters15@gmail.com>
2022-05-06 02:33:45 +0100

    Tweaks

---
+++ Hye - Indie Games Dev/Assets/Scripts/Attacks/WushiiBlastAttack.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WushiiBlastAttack : EnemyWeapon
+{
+    [Header("Attack Settings")]
+    bool beingUsed;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (beingUsed)
+        {
+            Quaternion targetRot = Quaternion.Euler(Quaternion.LookRotation(enemy.player.transform.position - enemy.transform.position, Vector3.up).eulerAngles + new Vector3(0, 90, 0));
+            enemy.rb.MoveRotation(Quaternion.Slerp(enemy.rb.rotation, targetRot, Time.deltaTime * 3f));
+        }
+    }
+
+    public void BlastStart()
+    {
+        beingUsed = true;
+    }
+
+    public void BlastFinished()
+    {
+        Finished();
+        beingUsed = false;
+    }
+    public override void Attack()
+    {
+        base.Attack();
+        attacking = false;
+    }
+}
